ad21e6516fe77ae36592d8f49209cfdaf03e6d58
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-30 13:49:04 -0400

    Add variable to target contact me

--- js/scroll.js
+++ js/scroll.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
 
   $("#contact-me-link").on("click", function(event){
     event.preventDefault();
+    var contactMe = $("#contact-me")[0]
+    console.log(contactMe);
   });
 });
